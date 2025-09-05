6110d237a2a7c84ddf48918199dd5b8c4f9bbeae
Bearry2626 <satang3223@gmail.com>
2025-09-05 14:24:11 +0700

    update weel05

--- week05/week05.js
+++ week05/week05.js
@@ -20,11 +20,22 @@ console.log([...num,...num1])
 console.log(num.toString())
 console.log(num.join("."))
 
-console.log("mangoteen".includes("go"))
-console.log(["mangoteen", "mango"].includes("go"))
-console.log(["mangoteen", "mango"].includes("Mango"))
+
+// includes
+console.log("mangoteen".includes("go")) // หาคำในString นี้
+console.log(["mangoteen", "mango"].includes("go")) // ไม่มีคำนี้ใน Arrays
+console.log(["mangoteen", "mango"].includes("Mango")) // case sensitive
 
 // reverse() - แก้ Original array 
 // sort() - default เป็นString เรียน case sensitive ตัวใหญ่ไปตัวเล็ก แล้วค่อยเรียนตัวเล็ก
+// กรณ๊เรียง String
+const monrgs2 = ['March', 'jan', 'feb', 'Dec']
+monrgs2.sort()
+console.log(monrgs2) //ถ้าใช้แบบนี้ธรรมดาเฉยๆ sort จะเรียงตาม unicode ให้
+// กรณีเรียง NUmber 
+const array14 = [1, 30, 4, 21, 100000]
+array14.sort()
+console.log(array14) //output จะเป็น [1, 100000, 21,30,4 ]
+
 // localeCompare - เปรียบเทียบข้อความ (string)
 
